ac1ebe28081b8c840c2006fc5ec4742ac67e50ed
hende <hendebii@gmail.com>
2020-07-17 15:48:23 +0300

    about done

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,61 +1,85 @@
 <template>
   <div>
     <h1 align="center" style="color: whitesmoke">About</h1>
-      <h1 style="color:white;">
-        <b>HAPPY RECIPES</b> started in the spring of 2020. Two hungy friends had a vision:
-        <br />Spread
-        <b>HAPPY COOCKING</b> by the students of Ben-Gurion University.
-        Our mission is to make a meal for a simple process.
-        <br />
-        <b>HAPPY RECIPES</b> features has an almost endless variety of recipes by using a
-        <b>Spoonacular</b> API.
-        <br />
-        <br />First, we wrote our
-        <a
-          href="https://app.swaggerhub.com/apis/hende8/Shiran-Hen/1.0.1#/user/post_user_myFavoriteRecipes"
-          class="card-link"
-        >API</a> in Swagger-hub.
-        <br />Once we had our API, we start in the mission and coded the backend side.
-        <br />You are more than welcome to take a look in our backend side
-        <a
-          href="https://github.com/SISE-Web-Development-Environments/assignment-3-2-205866106_204639173"
-          class="card-link"
-        >project</a>
-        <br />Having the backend side, we were highly motivated to complete the mission by coding the frontend side
-        <a
-          href="https://github.com/SISE-Web-Development-Environments/assignment-3-3-205866106_204639173"
-          class="card-link"
-        >project</a>
-        <br />Finally, we realy proud in our
-        <b>HAPPY RECIPES</b> website.
-        <br />
-        <br />
-        <br />
-      </h1>
-    <b-card-group deck>
-      <b-card
-        title="Shiran Golzar"
-        img-src="https://media-exp1.licdn.com/dms/image/C5603AQF1tDNKmnGTBQ/profile-displayphoto-shrink_400_400/0?e=1599696000&v=beta&t=4REba5t3-mKBaDmHIa0dNU_6YTw2_0-G14GYTlbYowk"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="max-width: 25rem;"
-        class="mb-2"
-      >
-        <b-card-text>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</b-card-text>
-      </b-card>
-      <b-card
-        title="Hen Debi"
-        img-src="https://media-exp1.licdn.com/dms/image/C4D03AQEnRAltWo4Qsw/profile-displayphoto-shrink_400_400/0?e=1599696000&v=beta&t=VnaoxirnyZ8vUJsMM75DAewobQRAepmwY_-wIQUVPU0"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="max-width: 25rem;"
-        class="mb-2"
-      >
-        <b-card-text>A third-year student in the Department of Software and Information Systems Engineering at Ben-Gurion University</b-card-text>
-      </b-card>
-    </b-card-group>
+    <h3 style="color:white;" align="center">
+      <b>HAPPY RECIPES</b> started in the spring of 2020. Two hungy friends had a vision:
+      <br />Spread
+      <b>HAPPY COOCKING</b> by the students of Ben-Gurion University.
+      Our mission is to make a meal for a simple process.
+      <br />
+      <b>HAPPY RECIPES</b> features has an almost endless variety of recipes by using a
+      <b>Spoonacular</b> API.
+      <br />
+      <br />First, we wrote our
+      <a
+        href="https://app.swaggerhub.com/apis/hende8/Shiran-Hen/1.0.1#/user/post_user_myFavoriteRecipes"
+        class="card-link"
+      >API</a> in Swagger-hub.
+      <br />Once we had our API, we start in the mission and coded the backend side.
+      <br />You are more than welcome to take a look in our backend side
+      <a
+        href="https://github.com/SISE-Web-Development-Environments/assignment-3-2-205866106_204639173"
+        class="card-link"
+      >project</a>
+      <br />Having the backend side, we were highly motivated to complete the mission by coding the frontend side
+      <a
+        href="https://github.com/SISE-Web-Development-Environments/assignment-3-3-205866106_204639173"
+        class="card-link"
+      >project</a>
+      <br />Finally, we realy proud in our
+      <b>HAPPY RECIPES</b> website.
+      <br />
+      <br />
+      <br />
+      <br />
+    </h3>
+    <h1 align="center" style="color: whitesmoke">The Crew</h1><br>
+    <br />
+    <div align="center" class="container">
+      <b-card-group deck>
+        <b-card
+          align="center"
+          title="Shiran Golzar"
+          img-src="https://media-exp1.licdn.com/dms/image/C5603AQF1tDNKmnGTBQ/profile-displayphoto-shrink_400_400/0?e=1599696000&v=beta&t=4REba5t3-mKBaDmHIa0dNU_6YTw2_0-G14GYTlbYowk"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 25rem;margin-right:17.5rem;"
+          class="mb-2"
+        >
+          <b-card-text>A third-year student in the Department of Software and Information Systems Engineering at Ben-Gurion University</b-card-text>
+          <a href="https://www.linkedin.com/in/shiran-golzar-4762b210a/">
+            <img src="https://img.icons8.com/doodle/48/000000/linkedin--v2.png" />
+          </a>
+          <a href="https://github.com/shirangol">
+            <img src="https://img.icons8.com/doodle/48/000000/github--v1.png" />
+          </a>
+          <a href="https://www.facebook.com/shiran.golzar">
+            <img src="https://img.icons8.com/doodle/48/000000/facebook-new.png" />
+          </a>
+        </b-card>
+        <b-card
+          title="Hen Debi"
+          img-src="https://media-exp1.licdn.com/dms/image/C4D03AQEnRAltWo4Qsw/profile-displayphoto-shrink_400_400/0?e=1599696000&v=beta&t=VnaoxirnyZ8vUJsMM75DAewobQRAepmwY_-wIQUVPU0"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 25rem;"
+          class="mb-2"
+        >
+          <b-card-text>A third-year student in the Department of Software and Information Systems Engineering at Ben-Gurion University</b-card-text>
+          <a href="https://www.linkedin.com/in/hen-debi-0547ba1a2/">
+            <img src="https://img.icons8.com/doodle/48/000000/linkedin--v2.png" />
+          </a>
+          <a href="https://github.com/hende8">
+            <img src="https://img.icons8.com/doodle/48/000000/github--v1.png" />
+          </a>
+          <a href="https://www.facebook.com/hen.debi">
+            <img src="https://img.icons8.com/doodle/48/000000/facebook-new.png" />
+          </a>
+        </b-card>
+      </b-card-group>
+    </div>
   </div>
 </template>
 
@@ -68,9 +92,9 @@ export default {
 </script>
 
  <style>
-/* .container {
-  max-width: 500px;
-  height: 50%;
-  color:whitesmoke;
-} */
+.card {
+        margin: 0 auto; /* Added */
+        float: none; /* Added */
+        margin-bottom: 10px; /* Added */
+}
 </style>
