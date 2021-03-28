459e470c57a5350bcb1d43050197e993c5db2333
Michal Talmor <talmormi@post.bgu.ac.il>
2021-03-28 10:15:58 +0300

    Evaluation

---
+++ src/components/EvaluationComponents/EvaluationByCounterfactual.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <h1>Explanation to be added</h1>
+
+    <b-button
+      @click="getevaluateByCounterfactual"
+      variant="primary"
+      style="width: 90px"
+      class="ml-5 w-10"
+      >Get graphs</b-button>
+    <div>
+      <b-card-group  deck v-for="img in images" :key="img">
+        <b-card no-body class="overflow-hidden" style="max-width: 2000px">
+          <b-row no-gutters>
+            <b-col md="6">
+              <b-card-img
+                :src="img"
+                alt="Image"
+                class="rounded-0"
+              ></b-card-img>
+            </b-col>
+            <b-col md="6">
+              <b-card-body title="Horizontal Card">
+                <b-card-text>
+                  This is a wider card with supporting text as a natural lead-in
+                  to additional content. This content is a little bit longer.
+                </b-card-text>
+              </b-card-body>
+            </b-col>
+          </b-row>
+        </b-card>
+      </b-card-group>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [],
+    };
+  },
+
+  methods: {
+    async getShapByErrors() {
+      // let info = await this.axios.get(
+      //   "http://localhost:8080/shap/GetShapByErrors/"
+      // );
+      // errors_plot = [];
+      // info.data.forEach((element) => {
+      //   errors_plot.push(element);
+      // });
+      this.images= ["https://picsum.photos/400/400/?image=20","https://picsum.photos/400/400/?image=20","https://picsum.photos/400/400/?image=20"]
+      console.log(this.images)
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/pages/EvaluationPage.vue
+++ src/pages/EvaluationPage.vue
@@ -1,36 +1,87 @@
 <template>
-<div>
-  <b-nav tabs>
-    <b-nav-item active>Clustering Intersection</b-nav-item>
-    <b-nav-item>Entropy</b-nav-item>
-    <b-nav-item>Counterfactual</b-nav-item>
-  </b-nav>
-  <br />
-  <br />
-  <br />
-  <br />
-  <br />
-  
-  <div class="text-center">
-  <b-spinner label="Spinning"></b-spinner>
-  <b-spinner type="grow" label="Spinning"></b-spinner>
-  <b-spinner variant="primary" label="Spinning"></b-spinner>
-  <b-spinner variant="primary" type="grow" label="Spinning"></b-spinner>
-  <b-spinner variant="success" label="Spinning"></b-spinner>
-  <b-spinner variant="success" type="grow" label="Spinning"></b-spinner>
-</div>
-</div>
+  <div>
+    <b-container>
+      <section class="text-center my-5">
+        <h2>
+          <b>Evaluation Tool</b>
+        </h2>
+        <p>Lets evaluate our explaination method</p>
+      </section>
+      <div>
+        <b-nav tabs fill>
+          <b-nav-item @click="swapComponents('EvaluationByClustering')"
+            >Clustering Intersection</b-nav-item
+          >
+          <b-nav-item @click="swapComponents('EvaluationByEntropy')"
+            >Evaluation By Entropy</b-nav-item
+          >
+          <b-nav-item @click="swapComponents('Counterfactual')"
+            >Counterfactual
+          </b-nav-item>
+          >
+        </b-nav>
+
+        <b-card-body v-if="current_component == 'EvaluationByClustering'">
+          <h1>Evaluation By Clustering</h1>
+        </b-card-body>
+        <b-card-body v-if="current_component == 'EvaluationByEntropy'">
+          <h1>Evaluation By Entropy</h1>
+          <EvaluationByEntropy class="SHAPSingleInstnace" />
+        </b-card-body>
+        <b-card-body v-if="current_component == 'EvaluationByCounterfactual'">
+          <h1>Evaluation By Counterfactual</h1>
+          <EvaluationByCounterfactual class="SHAPFullData" />
+        </b-card-body>
+      </div>
+    </b-container>
+    <b-container>
+      <side-bar>
+        <sidebar-item
+          :link="{
+            name: 'lime',
+            path: '/lime',
+          }"
+        >
+        </sidebar-item>
+        <sidebar-item
+          :link="{
+            name: 'shap',
+            path: '/shap',
+          }"
+        >
+        </sidebar-item>
+      </side-bar>
+    </b-container>
+  </div>
 </template>
+
 <script>
-export default {};
+import SideBar from "../components/SideBar.vue";
+// import EvaluationByClustering from "../components/EvaluationComponents/EvaluationByClustering.vue";
+import EvaluationByEntropy from "../components/EvaluationComponents/EvaluationByEntropy.vue";
+import EvaluationByCounterfactual from "../components/EvaluationComponents/EvaluationByCounterfactual.vue";
+export default {
+  components: { EvaluationByEntropy, EvaluationByCounterfactual },
+  data() {
+    return {
+      SideBar,
+      current_component: "EvaluationByClustering",
+    };
+  },
+  methods: {
+    swapComponents(componenet_name) {
+      this.current_component = componenet_name;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .inline-block {
   display: block;
 }
-.images{
+.images {
   margin-right: 20px;
-  display:inline-block
+  display: inline-block;
 }
 </style>
